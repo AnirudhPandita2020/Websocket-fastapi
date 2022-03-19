638b8334a670414006a629b59338ab169ec9d3bb
Anirudh <kppkanu@gmail.com>
2022-03-19 16:49:42 +0530

    fixed some bugs

--- app/main.py
+++ app/main.py
@@ -1,24 +1,67 @@
 
+import json
 from app.Notifer import ConnectionManager
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
-
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = window.location.pathname
+            var ws = new WebSocket(`ws://localhost:8000/ws`+client_id);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 app = FastAPI()
+
+@app.get("/{task_id}")
+async def get():
+    return HTMLResponse(html)
 
 
 manager = ConnectionManager()
 
 
+
 @app.websocket("/ws/{task_id}")
 async def websocket_endpoint(websocket: WebSocket,task_id:int):
     await manager.connect(websocket,task_id)
-    #print(task_id,manager.active_connections)
+
     try:
         while True:
             data = await websocket.receive_text()
-            
+            message_data = json.loads(data)
+            print(message_data['message'])
+
             await manager.broadcast(f"{data}",task_id)
     except WebSocketDisconnect:
+        print(websocket)
         manager.disconnect(websocket,task_id)
-        
         await manager.broadcast("left",task_id)
